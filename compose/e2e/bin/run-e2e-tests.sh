d9c284397d675154aa5837faeceb9466441e5daa
Teemu Hiltunen <teemu.hiltunen@gofore.com>
2021-12-30 11:15:03 +0200

    Added compose/

---
+++ compose/e2e/bin/run-e2e-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2017-2021 City of Oulu
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+#
+# Scavenged from evaka integration testing and e2e compose files
+
+set -euo pipefail
+
+export DEBUG="${DEBUG:-false}"
+export CI="${CI:-false}"
+export FORCE_COLOR=1
+export PROXY_URL=${PROXY_URL:-http://evaka-proxy:8080}
+
+if [ "${DEBUG}" = "true" ]; then
+    set -x
+fi
+
+export FORCE_COLOR=1
+
+cd /repo/evaka/frontend
+
+echo 'INFO: Waiting for compose stack to be up...'
+./wait-for-dev-api.sh "$PROXY_URL"
+
+cd /repo/frontend
+
+yarn e2e-ci-playwright
---
+++ compose/ecosystem.config.js
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2021 City of Oulu
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+const path = require('path')
+
+const defaults = {
+  autorestart: false
+}
+
+module.exports = {
+  apps: [{
+    name: 'apigw',
+    script: 'yarn && yarn clean && yarn dev',
+    cwd: path.resolve(__dirname, '../evaka/apigw'),
+    env: {
+      EVAKA_CUSTOMER_SAML_ENTRYPOINT: 'http://localhost:8080/auth/realms/citizens/protocol/saml',
+      EVAKA_CUSTOMER_SAML_ISSUER: 'evakaoulu',
+      EVAKA_SAML_ENTRYPOINT: 'http://localhost:8080/auth/realms/employees/protocol/saml',
+      EVAKA_SAML_ISSUER: 'evakaoulu',
+      AD_SAML_EXTERNAL_ID_PREFIX: 'oulu-ad'
+    },
+    ...defaults
+  }, {
+    name: 'frontend',
+    script: 'yarn && yarn clean && EVAKA_CUSTOMIZATIONS=oulu yarn dev',
+    cwd: path.resolve(__dirname, '../evaka/frontend'),
+    env: {
+      'ICONS': process.env.ICONS
+    },
+    ...defaults
+  }, {
+    name: 'service',
+    script: `${__dirname}/../evaka/compose/run-after-db.sh`,
+    args: './gradlew --no-daemon bootRun',
+    cwd: path.resolve(__dirname, '../service'),
+    ...defaults
+  },
+  ],
+}

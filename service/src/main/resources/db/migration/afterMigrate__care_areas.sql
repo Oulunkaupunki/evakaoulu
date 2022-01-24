0acd33a3c732964a785f2b62fbb25367c14517c5
Mikael Kulma <mikael.kulma@kipina.fi>
2022-01-24 11:39:15 +0200

    Update care areas migration script to include Oulu specific areas

--- service/src/main/resources/db/migration/afterMigrate__care_areas.sql
+++ service/src/main/resources/db/migration/afterMigrate__care_areas.sql
@@ -1,21 +1,35 @@
 -- SPDX-FileCopyrightText: 2021 City of Oulu
 --
 -- SPDX-License-Identifier: LGPL-2.1-or-later
-
 INSERT INTO care_area
-    (id, name, area_code, sub_cost_center, short_name)
+  (name, area_code, sub_cost_center, short_name)
 VALUES
-    ('6529e31e-9777-11eb-ba88-33a923255570', 'Etelä', 1, NULL, 'etela'),
-    ('6529f5a2-9777-11eb-ba89-cfcda122ed3b', 'Itä', 2, NULL, 'ita'),
-    ('6529f6ce-9777-11eb-ba8a-8f6495ec5104', 'Länsi', 3, NULL, 'lansi')
-ON CONFLICT (id) DO
+  ('Haukipudas', NULL, NULL, 'Haukipudas'),
+  ('Jääli', NULL, NULL, 'Jääli'),
+  ('Kaakkuri', NULL, NULL, 'Kaakkuri'),
+  ('Karjasilta-Höyhtyä', NULL, NULL, 'Karjasilta-Höyhtyä'),
+  ('Kastelli', NULL, NULL, 'Kastelli'),
+  ('Kaukovainio', NULL, NULL, 'Kaukovainio'),
+  ('Kello-Kiviniemi', NULL, NULL, 'Kello-Kiviniemi'),
+  ('Keskusta', NULL, NULL, 'Keskusta'),
+  ('Kiiminki', NULL, NULL, 'Kiiminki'),
+  ('Korvensuora', NULL, NULL, 'Korvensuora'),
+  ('Maikkula', NULL, NULL, 'Maikkula'),
+  ('Myllyoja', NULL, NULL, 'Myllyoja'),
+  ('Oulunsalo', NULL, NULL, 'Oulunsalo'),
+  ('Patenniemi-Rajakylä', NULL, NULL, 'Patenniemi-Rajakylä'),
+  ('Ritaharju-Kuivasjärvi-Pöllökangas', NULL, NULL, 'Ritaharju-Kuivasjärvi-Pöllökangas'),
+  ('Tuira-Toppila', NULL, NULL, 'Tuira-Toppila'),
+  ('Yli-Ii', NULL, NULL, 'Yli-Ii'),
+  ('Yli-Kiiminki', NULL, NULL, 'Yli-Kiiminki')
+ON CONFLICT (name) DO
 UPDATE SET
-    name = EXCLUDED.name,
-    area_code = EXCLUDED.area_code,
-    sub_cost_center = EXCLUDED.sub_cost_center,
-    short_name = EXCLUDED.short_name
+  name = EXCLUDED.name,
+  area_code = EXCLUDED.area_code,
+  sub_cost_center = EXCLUDED.sub_cost_center,
+  short_name = EXCLUDED.short_name
 WHERE
-    care_area.name <> EXCLUDED.name OR
-    care_area.area_code <> EXCLUDED.area_code OR
-    care_area.sub_cost_center <> EXCLUDED.sub_cost_center OR
-    care_area.short_name <> EXCLUDED.short_name;
+  care_area.name <> EXCLUDED.name OR
+  care_area.area_code <> EXCLUDED.area_code OR
+  care_area.sub_cost_center <> EXCLUDED.sub_cost_center OR
+  care_area.short_name <> EXCLUDED.short_name;

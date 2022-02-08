550f701ba20bde603bcf385accad55927d1f74bb
tahtee <timo.ahteela@gofore.com>
2022-02-08 13:41:10 +0200

    First half of Varhaiskasvatus (Oulu-27)

--- frontend/oulu/fiCustomizations.tsx
+++ frontend/oulu/fiCustomizations.tsx
@@ -54,6 +54,27 @@ const fi: DeepPartial<Translations> = {
     },
     editor: {
       unitPreference: {
+        siblingBasis: {
+          info: {
+            DAYCARE: (
+              <>
+                <P>
+                  Lapsella on sisarusperuste samaan varhaiskasvatuspaikkaan, 
+                  jossa hänen sisaruksensa on päätöksentekohetkellä. Tavoitteena 
+                  on sijoittaa sisarukset samaan varhaiskasvatuspaikkaan perheen 
+                  niin toivoessa. Jos haet paikkaa sisaruksille, jotka eivät 
+                  vielä ole varhaiskasvatuksessa, kirjoita tieto hakemuksen 
+                  Muut lisätiedot -kohtaan.
+                </P>
+                <P>
+                  Täytä nämä tiedot vain, jos käytät sisarusperustetta, sekä 
+                  valitse alla olevissa hakutoiveissa ensisijaiseksi toiveeksi 
+                  sama varhaiskasvatusyksikkö, jossa lapsen sisarus on.
+                </P>
+              </>
+            )
+          } 
+        },
         units: {
           serviceVoucherLink:
             'https://www.ouka.fi/oulu/palveluseteli/yksityisen-paivahoidon-palveluseteli'
@@ -64,28 +85,24 @@ const fi: DeepPartial<Translations> = {
           DAYCARE: (
             <>
               <P>
-                Varhaiskasvatuspaikkaa voi hakea ympäri vuoden.
-                Varhaiskasvatushakemus tulee jättää viimeistään neljä kuukautta
-                ennen hoidon toivottua alkamisajankohtaa. Mikäli
-                varhaiskasvatuksen tarve johtuu työllistymisestä, opinnoista tai
-                koulutuksesta, eikä hoidon tarpeen ajankohtaa ole pystynyt
-                ennakoimaan, on varhaiskasvatuspaikkaa haettava mahdollisimman
-                pian - kuitenkin viimeistään kaksi viikkoa ennen kuin lapsi
-                tarvitsee hoitopaikan.
+                Hakemus on jätettävä viimeistään neljä kuukautta ennen kuin 
+                tarvitsette paikan. Mikäli tarvitsette varhaiskasvatusta kiireellisesti 
+                työn tai opiskelujen vuoksi, käsittelyaika on kaksi viikkoa 
+                hakemuksen saapumisesta. 
               </P>
               <P>
-                Kirjallinen päätös varhaiskasvatuspaikasta lähetetään
-                Suomi.fi-viestit -palveluun. Mikäli haluatte päätöksen
-                sähköisenä tiedoksiantona, teidän tulee ottaa Suomi.fi-viestit
-                -palvelu käyttöön. Palvelusta ja sen käyttöönotosta saatte
+                Saatte kirjallisen päätöksen varhaiskasvatuspaikasta 
+                Suomi.fi-viestit -palveluun tai postitse, mikäli et ole 
+                ottanut Suomi.fi-palvelua käyttöön. Päätös on nähtävillä 
+                myös eVaka-palvelussa kohdassa Hakeminen - Päätökset.
+                Suomi.fi-viestit palvelusta ja sen käyttöönotosta saatte
                 lisätietoa{' '}
                 <ExternalLink
                   text="https://www.suomi.fi/viestit"
                   href="https://www.suomi.fi/viestit"
                   newTab
                 />
-                . Mikäli ette ota Suomi.fi-viestit -palvelua käyttöön, päätös
-                lähetetään teille postitse.
+                .
               </P>
               <P fitted={true}>* Tähdellä merkityt tiedot ovat pakollisia</P>
             </>
@@ -120,9 +137,10 @@ const fi: DeepPartial<Translations> = {
         startDate: {
           instructions: (
             <>
-              Toivottua aloituspäivää on mahdollista muuttaa myöhemmäksi niin
-              kauan, kun hakemusta ei ole otettu käsittelyyn. Tämän jälkeen
-              toivotun aloituspäivän muutokset tehdään ottamalla yhteyttä
+              Toivottua aloituspäivää on mahdollista muuttaa myöhemmäksi siihen 
+              saakka, kunnes palveluohjaus on ottanut hakemuksen käsittelyyn. 
+              Tämän jälkeen toivotun aloituspäivän muutokset tehdään ottamalla 
+              yhteyttä varhaiskasvatuksen palveluohjaukseen
               {customerContactText()}
             </>
           )
@@ -137,28 +155,61 @@ const fi: DeepPartial<Translations> = {
           attachmentsMessage: {
             text: (
               <P fitted={true}>
-                Mikäli varhaiskasvatuspaikan tarve johtuu äkillisestä
-                työllistymisestä tai opiskelupaikan saamisesta, tulee paikkaa
-                hakea viimeistään kaksi viikkoa ennen kuin hoidon tarve alkaa.
-                Lisäksi huoltajan tulee ottaa yhteyttä viipymättä
-                {customerContactText()}
+                Mikäli varhaiskasvatuspaikan tarve johtuu äkillisestä 
+                työllistymisestä tai opiskelusta, tulee paikkaa hakea 
+                viimeistään kaksi viikkoa ennen kuin tarve alkaa. 
+                Hakemuksen liitteenä tulee olla selvitys työ- tai 
+                opiskelupaikasta molemmilta samassa taloudessa asuvilta 
+                huoltajilta.  Kahden viikon käsittelyaika alkaa siitä, 
+                kun olemme vastaanottaneet hakemuksen tarvittavine 
+                liitteineen. Jos et voi lisätä liitteitä hakemukselle 
+                sähköisesti, lähetä ne sähköpostilla osoitteeseen{' '}
+                <a href="mailto:varhaiskasvatus@ouka.fi">varhaiskasvatus@ouka.fi</a>
+                {' '}tai postilla osoitteeseen 
+                Varhaiskasvatuksen palveluohjaus, PL 75, 90015 Oulun kaupunki.
               </P>
             )
           }
         },
         shiftCare: {
           instructions:
-            'Päiväkodit palvelevat normaalisti arkisin klo 6.00–18.00. Iltahoito on tarkoitettu lapsille, jotka vanhempien työn tai tutkintoon johtavan opiskelun vuoksi tarvitsevat säännöllisesti hoitoa klo 18.00 jälkeen. Iltahoitoa tarjoavat päiväkodit aukeavat tarvittaessa klo 5.30 ja menevät kiinni viimeistään klo 22.30. Osa iltahoitoa antavista päiväkodeista on auki myös viikonloppuisin. Vuorohoito on tarkoitettu lapsille, joiden vanhemmat tekevät vuorotyötä ja lapsen hoitoon sisältyy myös öitä.',
+            'Ilta- ja vuorohoidolla tarkoitetaan pääasiassa klo 6.00-18.00 ulkopuolella ja viikonloppuisin sekä ympärivuorokautisesti tapahtuvaa varhaiskasvatusta. Mikäli tarvitset ilta- tai vuorohoitoa, täsmennä tarvetta hakemuksen Muut lisätiedot -kohdassa.',
           message: {
-            text: 'Ilta- ja vuorohoito on tarkoitettu lapsille, jotka vanhempien työn tai tutkintoon johtavan opiskelun vuoksi tarvitsevat ilta- ja vuorohoitoa. Hakemuksen liitteeksi on toimitettava vanhempien osalta työnantajan todistus vuorotyöstä tai opiskelusta johtuvasta ilta- tai vuorohoidon tarpeesta.'
+            text: (
+              <P fitted={true}>
+                Ilta- ja vuorohoito on tarkoitettu lapsille, joiden molemmat 
+                vanhemmat ovat vuorotyössä tai opiskelevat pääsääntöisesti 
+                iltaisin ja/tai viikonloppuisin. Hakemuksen liitteenä tulee 
+                olla selvitys vuorotyöstä tai iltaisin ja/tai viikonloppuisin 
+                tapahtuvasta opiskelusta molemmilta samassa taloudessa asuvilta 
+                huoltajilta. Jos et voi lisätä liitteitä hakemukselle 
+                sähköisesti, lähetä ne sähköpostilla osoitteeseen{' '}
+                <a href="mailto:varhaiskasvatus@ouka.fi">varhaiskasvatus@ouka.fi</a>
+                {' '}tai postilla osoitteeseen 
+                Varhaiskasvatuksen palveluohjaus, PL 75, 90015 Oulun kaupunki.
+              </P>
+            )
           },
           attachmentsMessage: {
-            text: 'Ilta- ja vuorohoito on tarkoitettu lapsille, jotka vanhempien työn tai tutkintoon johtavan opiskelun vuoksi tarvitsevat ilta- ja vuorohoitoa. Hakemuksen liitteeksi on toimitettava vanhempien osalta työnantajan todistus vuorotyöstä tai opiskelusta johtuvasta ilta- tai vuorohoidon tarpeesta.'
+            text: (
+              <P fitted={true}>
+                Ilta- ja vuorohoito on tarkoitettu lapsille, joiden molemmat 
+                vanhemmat ovat vuorotyössä tai opiskelevat pääsääntöisesti 
+                iltaisin ja/tai viikonloppuisin. Hakemuksen liitteenä tulee 
+                olla selvitys vuorotyöstä tai iltaisin ja/tai viikonloppuisin 
+                tapahtuvasta opiskelusta molemmilta samassa taloudessa asuvilta 
+                huoltajilta. Jos et voi lisätä liitteitä hakemukselle 
+                sähköisesti, lähetä ne sähköpostilla osoitteeseen{' '}
+                <a href="mailto:varhaiskasvatus@ouka.fi">varhaiskasvatus@ouka.fi</a>
+                {' '}tai postilla osoitteeseen 
+                Varhaiskasvatuksen palveluohjaus, PL 75, 90015 Oulun kaupunki.
+              </P>
+            )
           }
         },
         assistanceNeedInstructions: {
           DAYCARE:
-            'Tehostettua tai erityistä tukea annetaan lapselle heti tarpeen ilmettyä. Mikäli lapsella on olemassa tuen tarpeesta asiantuntijalausunto, tämä tulee ilmoittaa varhaiskasvatushakemuksella. Tukitoimet toteutuvat lapsen arjessa osana varhaiskasvatuksen toimintaa. Oulun varhaiskasvatuksesta otetaan erikseen yhteyttä hakemuksen jättämisen jälkeen, jos lapsella on tuen tarve.',
+            'Lapsen tuen tarpeella tarkoitetaan sellaisten tukitoimien tarvetta, jotka on osoitettu asiantuntijalausunnoin. Tukitoimet toteutuvat lapsen arjessa osana varhaiskasvatuksen muuta toimintaa. Varhaiskasvatuksen erityisopettaja ottaa hakijaan yhteyttä, jotta lapsen tuen tarve voidaan ottaa huomioon varhaiskasvatuspaikkaa myönnettäessä.',
           CLUB: 'Jos lapsella on tuen tarve, Oulun varhaiskasvatuksesta otetaan yhteyttä hakemuksen jättämisen jälkeen.'
         },
         partTime: {

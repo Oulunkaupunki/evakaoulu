ec8de1d2ea07eb3bc7eb3822e5d0672869729123
Mikael Kulma <mikael.kulma@kipina.fi>
2022-02-10 08:46:37 +0200

    Update theme to match core changes

--- frontend/oulu/oulu-theme.tsx
+++ frontend/oulu/oulu-theme.tsx
@@ -10,39 +10,39 @@ import { Theme } from 'lib-common/theme'
 
 const theme: Theme = {
   colors: {
-    brand: {
-      primary: '#00417D',
-      secondary: '#39A7D7',
-      secondaryLight: '#E5EEF8'
-    },
     main: {
-      dark: '#00417D',
-      medium: '#4676BE',
-      primary: '#29549A',
-      primaryHover: '#4676BE',
-      primaryActive: '#00417D',
-      light: '#CAD6E2',
-      lighter: '#E5EEF8'
+      m1: '#00417D',
+      m2: '#29549A',
+      m3: '#4676BE',
+      m4: '#E5EEF8',
+      m2Hover: '#00417D',
+      m2Active: '#00417D',
+      m2Focus: '#4676BE'
     },
-    greyscale: {
-      darkest: '#0F0F0F',
-      dark: '#585857',
-      medium: '#B1B1B1',
-      lighter: '#D8D8D8',
-      lightest: '#F5F5F5',
-      white: '#FFFFFF'
+    grayscale: {
+      g100: '#0F0F0F',
+      g70: '#585857',
+      g35: '#B1B1B1',
+      g15: '#D8D8D8',
+      g4: '#F5F5F5',
+      g0: '#FFFFFF'
+    },
+    status: {
+      danger: '#CB181A',
+      warning: '#EB6A00',
+      success: '#799E3D',
+      info: '#5390B5'
     },
     accents: {
-      orange: '#FF7300',
-      orangeDark: '#B85300',
-      green: '#92BB4F',
-      greenDark: '#3C6100',
-      water: '#9FC1D3',
-      yellow: '#F4D240',
-      red: '#CB181A',
-      petrol: '#5390B5',
-      emerald: '#397368',
-      violet: '#7361A2'
+      a1greenDark: '#3C6100',
+      a2orangeDark: '#B85300',
+      a3emerald: '#397368',
+      a4violet: '#7361A2',
+      a5orangeLight: '#F4D240',
+      a6turquoise: '#91C9EA',
+      a7mint: '#8CC1B3',
+      a8lightBlue: '#CAD6E2',
+      a9pink: '#D77094'
     }
   },
   typography: {
@@ -68,13 +68,6 @@ const theme: Theme = {
       }
     },
     h4: {
-      weight: 400,
-      bold: 600,
-      mobile: {
-        weight: 500
-      }
-    },
-    h5: {
       weight: 400,
       bold: 600,
       mobile: {

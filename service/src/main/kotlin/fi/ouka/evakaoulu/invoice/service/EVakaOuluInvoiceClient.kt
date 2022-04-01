dee6c3f8728a14516b005bb2066b6fdd4695ff5e
tahtee <timo.ahteela@gofore.com>
2022-04-01 15:09:20 +0300

    Transfer billing starting

--- service/src/main/kotlin/fi/ouka/evakaoulu/invoice/service/EVakaOuluInvoiceClient.kt
+++ service/src/main/kotlin/fi/ouka/evakaoulu/invoice/service/EVakaOuluInvoiceClient.kt
@@ -4,14 +4,39 @@
 
 package fi.ouka.evakaoulu.invoice.service
 
+import com.jcraft.jsch.ChannelSftp
+import com.jcraft.jsch.JSch
+import com.jcraft.jsch.Session
 import fi.espoo.evaka.invoicing.domain.InvoiceDetailed
 import fi.espoo.evaka.invoicing.integration.InvoiceIntegrationClient
 import fi.ouka.evakaoulu.IntimeProperties
+import org.apache.tomcat.jni.User.username
+
+
+
 
 class EVakaOuluInvoiceClient(
     private val properties: IntimeProperties
 ) : InvoiceIntegrationClient {
     override fun send(invoices: List<InvoiceDetailed>): InvoiceIntegrationClient.SendResult {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        val channelSftp: ChannelSftp = setupJsch()
+        channelSftp.connect()
+        val localFile = "src/main/resources/sample.txt"
+        val remoteDir = properties.address
+        channelSftp.put(localFile, remoteDir + "jschFile.txt")
+        channelSftp.exit()
     }
+
+//    @Throws(JSchException::class)
+    private fun setupJsch(): ChannelSftp? {
+        val jsch = JSch()
+        jsch.setKnownHosts("/Users/john/.ssh/known_hosts")
+        val jschSession: Session = jsch.getSession(properties.username, properties.address)
+        jschSession.setPassword(properties.password)
+        jschSession.connect()
+        return jschSession.openChannel("sftp") as ChannelSftp
+    }
+
+
 }

544170dbe5eed2e465fe5f1aff829a4b0045e985
Mikael Kulma <mikael.kulma@kipina.fi>
2022-04-19 14:11:48 +0300

    Fix invoicesender test

--- service/src/main/kotlin/fi/ouka/evakaoulu/invoice/service/InvoiceSender.kt
+++ service/src/main/kotlin/fi/ouka/evakaoulu/invoice/service/InvoiceSender.kt
@@ -1,18 +1,13 @@
 package fi.ouka.evakaoulu.invoice.service
 
-import com.jcraft.jsch.ChannelSftp
-import com.jcraft.jsch.JSch
-import com.jcraft.jsch.Session
 import fi.ouka.evakaoulu.IntimeProperties
 import java.io.ByteArrayInputStream
 
 class InvoiceSender(val intimeProperties: IntimeProperties, val sftpConnector: SftpConnector) {
-    fun send(proEInvoices: List<String>): Unit {
+    fun send(proEInvoices: List<String>) {
         print(proEInvoices)
         sftpConnector.connect(intimeProperties.address, intimeProperties.username, intimeProperties.password)
         proEInvoices.forEach{sftpConnector.send(intimeProperties.path, ByteArrayInputStream(it.toByteArray()))}
         sftpConnector.disconnect()
     }
-
-
 }
--- service/src/test/kotlin/fi/ouka/evakaoulu/invoice/service/InvoiceSenderTest.kt
+++ service/src/test/kotlin/fi/ouka/evakaoulu/invoice/service/InvoiceSenderTest.kt
@@ -1,15 +1,8 @@
 package fi.ouka.evakaoulu.invoice.service
 
-import com.jcraft.jsch.ChannelSftp
-import com.jcraft.jsch.JSch
-import com.jcraft.jsch.Session
 import fi.ouka.evakaoulu.IntimeProperties
-import org.hamcrest.Matchers
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
+import org.mockito.kotlin.*
 import java.io.ByteArrayInputStream
 
 internal class InvoiceSenderTest {
@@ -18,14 +11,12 @@ internal class InvoiceSenderTest {
         val intimeProperties = IntimeProperties("", "", "", "")
         val sftpConnector = mock<SftpConnector>()
         val invoiceSender = InvoiceSender(intimeProperties, sftpConnector)
+        val proEInvoices = listOf("one", "two")
 
-        val proEInvoices = listOf("Hauskaa", "Tosiaan")
         invoiceSender.send(proEInvoices)
 
         verify(sftpConnector).connect(intimeProperties.address, intimeProperties.username, intimeProperties.password)
-//        proEInvoices.forEach{
-//            verify(sftpConnector).send(intimeProperties.path, any() )
-//        }
+        verify(sftpConnector, times(2)).send(eq(intimeProperties.path), any<ByteArrayInputStream>())
         verify(sftpConnector).disconnect()
     }
 }

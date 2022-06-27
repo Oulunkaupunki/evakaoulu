614be0fe0ee44f31f33ace21cf2d583fce94a402
tahtee <timo.ahteela@gofore.com>
2022-06-27 11:55:51 +0300

    Mobiilissa_vaara_poissaolonimi-426

--- frontend/oulu/employeeMobile.tsx
+++ frontend/oulu/employeeMobile.tsx
@@ -16,6 +16,34 @@ const customizations: EmployeeMobileCustomizations = {
     fi: {
       mobile: {
         landerText1: 'Tervetuloa käyttämään eVaka Oulu -mobiilisovellusta!'
+      },
+      absences: {
+        title: 'Poissaolomerkintä',
+        absenceTypes: {
+          OTHER_ABSENCE: 'Poissaolo',
+          SICKLEAVE: 'Sairaus',
+          UNKNOWN_ABSENCE: 'Ilmoittamaton poissaolo',
+          PLANNED_ABSENCE: 'Sopimuspoissaolo',
+          TEMPORARY_RELOCATION: 'Lapsi varasijoitettuna muualla',
+          PARENTLEAVE: 'Isyysvapaa',
+          FORCE_MAJEURE: 'Hyvityspäivä',
+          FREE_ABSENCE: 'Maksuton poissaolo',
+          NO_ABSENCE: 'Ei poissaoloa'
+        },
+        careTypes: {
+          SCHOOL_SHIFT_CARE: 'Koululaisten vuorohoito',
+          PRESCHOOL: 'Esiopetus',
+          PRESCHOOL_DAYCARE: 'Liittyvä varhaiskasvatus',
+          DAYCARE_5YO_FREE: '5-vuotiaiden varhaiskasvatus',
+          DAYCARE: 'Varhaiskasvatus',
+          CLUB: 'Kerho'
+        },
+        chooseStartDate: 'Valitse tuleva päivä',
+        startBeforeEnd: 'Aloitus oltava ennen päättymispäivää.',
+        reason: 'Poissaolon syy',
+        fullDayHint: 'Poissaolomerkintä tehdään koko päivälle',
+        confirmDelete: 'Haluatko poistaa tämän poissaolon?',
+        futureAbsence: 'Tulevat poissaolot'
       }
     }
   },

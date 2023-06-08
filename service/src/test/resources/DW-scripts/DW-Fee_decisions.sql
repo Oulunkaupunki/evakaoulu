6828e08d8d0bf3e776210f4a80d4e09b87379bca
Mikko Savolainen <mikko.savolainen@gofore.com>
2023-06-08 12:52:18 +0300

    add DW script for fee decision data

---
+++ service/src/test/resources/DW-scripts/DW-Fee_decisions.sql
@@ -0,0 +1,19 @@
+SELECT
+    fd.decision_number AS maksupäätöksen_numero,
+    lower(fd.valid_during) AS alkupvm,
+    upper(fd.valid_during) AS loppupvm,
+    fd.decision_type AS huojennustyyppi,
+    fd.family_size AS perhekoko,
+    fdc.child_id AS lapsen_id,
+    fdc.final_fee AS lapsikohtainen_maksu,
+    fdc.placement_type AS toimintamuoto,
+    ca.name AS palvelualue,
+    d.name AS toimipaikka,
+    d.dw_cost_center AS kustannuspaikka
+FROM fee_decision fd
+    JOIN fee_decision_child fdc on fd.id = fdc.fee_decision_id
+    JOIN daycare d ON fdc.placement_unit_id = d.id
+    JOIN care_area ca ON d.care_area_id = ca.id
+WHERE fd.status = 'SENT'
+    AND fd.decision_number IS NOT NULL -- ei tuoda effican päätöksiä
+    AND current_date - INTERVAL '3 months' <= upper(fd.valid_during);

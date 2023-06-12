62a81da96f6c88556ec70acb6ed8ce0226d489f0
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2023-06-12 16:20:06 +0300

    Add test script to upload an empty file to the Oulu DW sftp

--- scheduled-tasks/reporting/db_to_csv.sh
+++ scheduled-tasks/reporting/db_to_csv.sh
@@ -2,3 +2,13 @@
 
 # PRINT SOMETHING for testing (should contain DW report exporting logic)
 echo "Hello world!"
+
+DATE=`date +%Y%m%d`
+FILE=test_$DATE.txt
+touch $FILE
+
+echo "put $FILE" > sftp_batch.txt
+echo "bye" >> sftp_batch.txt
+
+export SSHPASS=$SFTP_PASS
+sshpass -e sftp -o StrictHostKeyChecking=no -o BatchMode=no -b sftp_batch.txt $SFTP_USER@$SFTP_HOST:$SFTP_PATH

c9d2c81ba4cf27f1fa0dd9a2e6b78d9ba9a417cf
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2023-06-13 10:26:45 +0300

    Refactor sending the DW file into a function in the script

--- scheduled-tasks/reporting/db_to_csv.sh
+++ scheduled-tasks/reporting/db_to_csv.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+export SSHPASS=$SFTP_PASS
+
+send_file() {
+    FILE=$1
+
+    echo "put $FILE" > sftp_batch.txt
+    echo "bye" >> sftp_batch.txt
+
+    sshpass -e sftp -o StrictHostKeyChecking=no -o BatchMode=no -b sftp_batch.txt $SFTP_USER@$SFTP_HOST:$SFTP_PATH
+    aws s3 cp $FILE s3://$S3_BUCKET/$FILE
+}
+
 # PRINT SOMETHING for testing (should contain DW report exporting logic)
 echo "Hello world!"
 
@@ -7,9 +19,4 @@ DATE=`date +%Y%m%d`
 FILE=test_$DATE.txt
 touch $FILE
 
-echo "put $FILE" > sftp_batch.txt
-echo "bye" >> sftp_batch.txt
-
-export SSHPASS=$SFTP_PASS
-sshpass -e sftp -o StrictHostKeyChecking=no -o BatchMode=no -b sftp_batch.txt $SFTP_USER@$SFTP_HOST:$SFTP_PATH
-aws s3 cp $FILE s3://$S3_BUCKET/$FILE
+send_file $FILE

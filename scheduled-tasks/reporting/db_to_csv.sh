91b0ad552165e02583b2534318befe30fccc00f3
Mikko Savolainen <mikko.savolainen@gofore.com>
2023-06-16 14:00:54 +0300

    Add DW report runs to scheduled reporting script

--- scheduled-tasks/reporting/db_to_csv.sh
+++ scheduled-tasks/reporting/db_to_csv.sh
@@ -8,15 +8,43 @@ send_file() {
     echo "put $FILE" > sftp_batch.txt
     echo "bye" >> sftp_batch.txt
 
-    sshpass -e sftp -o StrictHostKeyChecking=no -o BatchMode=no -b sftp_batch.txt $SFTP_USER@$SFTP_HOST:$SFTP_PATH
-    aws s3 cp $FILE s3://$S3_BUCKET/$FILE
+    sshpass -e sftp -o StrictHostKeyChecking=no -o BatchMode=no -b sftp_batch.txt "$SFTP_USER"@"$SFTP_HOST":"$SFTP_PATH"
+    aws s3 cp "$FILE" s3://"$S3_BUCKET"/"$FILE"
 }
 
-# PRINT SOMETHING for testing (should contain DW report exporting logic)
-echo "Hello world!"
+DATE=$(date --iso-8601)
 
-DATE=`date +%Y%m%d`
-FILE=test_$DATE.txt
-touch $FILE
+# Daily info
 
-send_file $FILE
+DAILY_QUERY=$(cat sql/DW-Daily-info.sql)
+DAILY_FILE=Daily_info_$DATE.csv
+psql -c "COPY $DAILY_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$DAILY_FILE"
+send_file "$DAILY_FILE"
+
+# Assistance need decisions
+
+ASSISTANCE_QUERY=$(cat sql/DW-Assistance_need_decisions.sql)
+ASSISTANCE_FILE=Daily_info_$DATE.csv
+psql -c "COPY $ASSISTANCE_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$ASSISTANCE_FILE"
+send_file "$ASSISTANCE_FILE"
+
+# Units and groups
+
+UNIT_QUERY=$(cat sql/DW-Unite_and_groups.sql)
+UNIT_FILE=Daily_info_$DATE.csv
+psql -c "COPY $UNIT_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$UNIT_FILE"
+send_file "$UNIT_FILE"
+
+# Fee decisions
+
+FEE_QUERY=$(cat sql/DW-Fee_decisions.sql)
+FEE_FILE=Daily_info_$DATE.csv
+psql -c "COPY $FEE_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$FEE_FILE"
+send_file "$FEE_FILE"
+
+# Voucher value decisions
+
+VOUCHER_QUERY=$(cat sql/DW-Voucher_value_decisions.sql)
+VOUCHER_FILE=Daily_info_$DATE.csv
+psql -c "COPY $VOUCHER_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$VOUCHER_FILE"
+send_file "$VOUCHER_FILE"

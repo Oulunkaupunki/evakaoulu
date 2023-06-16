d8ad2458127f398d95daed705339fede5259c455
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2023-06-16 15:13:33 +0300

    Fix running the SQL queries in the DW script
    The COPY TO STDOUT method doesn't seem to work if the scripts has
    anything else than the SELECT statement, had to redirect the SQL
    files to psql instead to make them work

--- scheduled-tasks/reporting/db_to_csv.sh
+++ scheduled-tasks/reporting/db_to_csv.sh
@@ -2,49 +2,35 @@
 
 export SSHPASS=$SFTP_PASS
 
+run_query() {
+    SQL_FILE=$1
+    OUTPUT_FILE=$2
+
+    psql --csv < $SQL_FILE > $OUTPUT_FILE
+}
+
 send_file() {
     FILE=$1
 
     echo "put $FILE" > sftp_batch.txt
     echo "bye" >> sftp_batch.txt
 
-    sshpass -e sftp -o StrictHostKeyChecking=no -o BatchMode=no -b sftp_batch.txt "$SFTP_USER"@"$SFTP_HOST":"$SFTP_PATH"
-    aws s3 cp "$FILE" s3://"$S3_BUCKET"/"$FILE"
+    sshpass -e sftp -o StrictHostKeyChecking=no -o BatchMode=no -b sftp_batch.txt $SFTP_USER@$SFTP_HOST:$SFTP_PATH
+    aws s3 cp $FILE s3://$S3_BUCKET/$FILE
+}
+
+run_query_and_send_file() {
+    SQL_FILE=$1
+    OUTPUT_FILE=$2
+
+    run_query $SQL_FILE $OUTPUT_FILE
+    send_file $OUTPUT_FILE
 }
 
 DATE=$(date --iso-8601)
 
-# Daily info
-
-DAILY_QUERY=$(cat sql/DW-Daily-info.sql)
-DAILY_FILE=Daily_info_$DATE.csv
-psql -c "COPY $DAILY_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$DAILY_FILE"
-send_file "$DAILY_FILE"
-
-# Assistance need decisions
-
-ASSISTANCE_QUERY=$(cat sql/DW-Assistance_need_decisions.sql)
-ASSISTANCE_FILE=Daily_info_$DATE.csv
-psql -c "COPY $ASSISTANCE_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$ASSISTANCE_FILE"
-send_file "$ASSISTANCE_FILE"
-
-# Units and groups
-
-UNIT_QUERY=$(cat sql/DW-Unite_and_groups.sql)
-UNIT_FILE=Daily_info_$DATE.csv
-psql -c "COPY $UNIT_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$UNIT_FILE"
-send_file "$UNIT_FILE"
-
-# Fee decisions
-
-FEE_QUERY=$(cat sql/DW-Fee_decisions.sql)
-FEE_FILE=Daily_info_$DATE.csv
-psql -c "COPY $FEE_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$FEE_FILE"
-send_file "$FEE_FILE"
-
-# Voucher value decisions
-
-VOUCHER_QUERY=$(cat sql/DW-Voucher_value_decisions.sql)
-VOUCHER_FILE=Daily_info_$DATE.csv
-psql -c "COPY $VOUCHER_QUERY TO STDOUT WITH DELIMITER ',' CSV HEADER;" > "$VOUCHER_FILE"
-send_file "$VOUCHER_FILE"
+run_query_and_send_file sql/DW-Daily_info.sql daily_info_$DATE.csv
+run_query_and_send_file sql/DW-Units_and_groups.sql units_and_groups_$DATE.csv
+run_query_and_send_file sql/DW-Fee_decisions.sql fee_decisions_$DATE.csv
+run_query_and_send_file sql/DW-Voucher_value_decisions.sql voucher_value_decisions_$DATE.csv
+run_query_and_send_file sql/DW-Assistance_need_decisions.sql assistance_need_decisions_$DATE.csv

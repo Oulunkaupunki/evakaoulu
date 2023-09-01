badffba23d2608d5cf91b23437ee94004b0ac7ae
Martin Lehtomaa <martin.lehtomaa@rebase.fi>
2023-09-01 11:56:18 +0300

    Added child discussion query for DW reporting

---
+++ scheduled-tasks/reporting/sql/DW-Child_discussions.sql
@@ -0,0 +1,11 @@
+SET TIMEZONE = 'Europe/Helsinki';
+SELECT
+    now()           AS aikaleima,
+    id,
+    child_id 		AS lapsen_id,
+    created 		AS luonti_aika,
+    offered_date 	AS tarjottu_pvm,
+    held_date 		AS pidetty_pvm,
+    counseling_date AS neuvonpito_pvm
+FROM child_discussion
+WHERE :date_val::DATE - INTERVAL '3 months' <= DATE(created);

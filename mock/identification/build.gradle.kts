3d54c9cdcf975d319c12781d20376aeae95e881b
Martin Lehtomaa <48860407+martinLeht@users.noreply.github.com>
2023-09-22 14:42:28 +0300

    Keycloak update v22 (#59)
    * keycloak update 16->22
    * updated mock/identification 
    ---------
    
    Co-authored-by: mrosin <martin.rosin.fi@gmail.com>

--- mock/identification/build.gradle.kts
+++ mock/identification/build.gradle.kts
@@ -5,11 +5,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("io.freefair.lombok") version "6.3.0"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.6.0"
+    id("org.springframework.boot") version "2.7.3"
+    id("io.freefair.lombok") version "8.3"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
 }
 
 group = "com.example"
@@ -22,41 +21,12 @@ repositories {
     maven(url = "https://build.shibboleth.net/nexus/content/repositories/releases/")
 }
 
-dependencyManagement {
-    imports {
-        mavenBom("org.apereo.cas:cas-server-support-bom:6.4.4")
-    }
-}
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // minimal cas installation from https://github.com/apereo/cas/blob/v6.4.4/gradle/webapp.gradle
-    implementation("org.apereo.cas:cas-server-core")
-    implementation("org.apereo.cas:cas-server-core-audit")
-    implementation("org.apereo.cas:cas-server-core-authentication")
-    implementation("org.apereo.cas:cas-server-core-configuration")
-    implementation("org.apereo.cas:cas-server-core-cookie")
-    implementation("org.apereo.cas:cas-server-core-logout")
-    implementation("org.apereo.cas:cas-server-core-logging")
-    implementation("org.apereo.cas:cas-server-core-services")
-    implementation("org.apereo.cas:cas-server-core-tickets")
-    implementation("org.apereo.cas:cas-server-core-util")
-    implementation("org.apereo.cas:cas-server-core-validation")
-    implementation("org.apereo.cas:cas-server-core-web")
-    implementation("org.apereo.cas:cas-server-core-notifications")
-    implementation("org.apereo.cas:cas-server-support-jpa-util")
-    implementation("org.apereo.cas:cas-server-support-actions")
-    implementation("org.apereo.cas:cas-server-support-person-directory")
-    implementation("org.apereo.cas:cas-server-support-themes")
-    implementation("org.apereo.cas:cas-server-support-validation")
-    implementation("org.apereo.cas:cas-server-support-thymeleaf")
-    implementation("org.apereo.cas:cas-server-support-pm-webflow")
-    implementation("org.apereo.cas:cas-server-webapp-config")
-    implementation("org.apereo.cas:cas-server-webapp-init")
-    implementation("org.apereo.cas:cas-server-webapp-resources")
-    implementation("org.apereo.cas:cas-server-webapp-init-tomcat")
+    implementation(platform("org.apereo.cas:cas-server-support-bom:6.6.10"))
+    implementation("org.apereo.cas:cas-server-webapp-starter-tomcat")
 
     implementation("org.apereo.cas:cas-server-support-generic")
     implementation("org.apereo.cas:cas-server-support-yaml-service-registry")
@@ -72,6 +42,24 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.register("resolveDependencies") {
+    description = "Resolves all dependencies"
+    doLast {
+        configurations
+            .matching { it.isCanBeResolved }
+            .map {
+                val files = it.resolve()
+                it.name to files.size
+            }
+            .groupBy({ (_, count) -> count }) { (name, _) -> name }
+            .forEach { (count, names) ->
+                println(
+                    "Resolved $count dependency files for configurations: ${names.joinToString(", ")}",
+                )
+            }
+    }
 }
 
 springBoot {

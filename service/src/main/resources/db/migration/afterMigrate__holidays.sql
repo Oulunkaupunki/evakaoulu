7c9f4f96d60edece022d47df9778f00c1d26e3bd
tahtee <96183234+tahtee@users.noreply.github.com>
2023-11-10 08:16:46 +0200

    Added year 2024 and 2025 holidays to eVaka

--- service/src/main/resources/db/migration/afterMigrate__holidays.sql
+++ service/src/main/resources/db/migration/afterMigrate__holidays.sql
@@ -5,6 +5,38 @@
 INSERT INTO holiday
 (date, description)
 VALUES
+    -- 2025
+    ('2025-01-01',' uudenvuodenpäivä'),
+    ('2025-01-06',' loppiainen'),
+    ('2025-04-18',' pitkäperjantai'),
+    ('2025-04-20',' pääsiäispäivä'),
+    ('2025-04-21',' toinen pääsiäispäivä'),
+    ('2025-05-01',' vappu'),
+    ('2025-05-29',' helatorstai'),
+    ('2025-06-08',' helluntai'),
+    ('2025-06-20',' juhannusaatto'),
+    ('2025-06-21',' juhannuspäivä'),
+    ('2025-11-01',' pyhäinpäivä'),
+    ('2025-12-06',' itsenäisyyspäivä'),
+    ('2025-12-24',' jouluaatto'),
+    ('2025-12-25',' joulupäivä'),
+    ('2025-12-26',' tapaninpäivä'),
+    -- 2024
+    ('2024-01-01',' uudenvuodenpäivä'),
+    ('2024-01-06',' loppiainen'),
+    ('2024-03-29',' pitkäperjantai'),
+    ('2024-03-31',' pääsiäispäivä'),
+    ('2024-04-01',' toinen pääsiäispäivä'),
+    ('2024-05-01',' vappu'),
+    ('2024-05-09',' helatorstai'),
+    ('2024-05-19',' helluntai'),
+    ('2024-06-21',' juhannusaatto'),
+    ('2024-06-22',' juhannuspäivä'),
+    ('2024-11-02',' pyhäinpäivä'),
+    ('2024-12-06',' itsenäisyyspäivä'),
+    ('2024-12-24',' jouluaatto'),
+    ('2024-12-25',' joulupäivä'),
+    ('2024-12-26',' tapaninpäivä'),
     -- 2023
     ('2023-01-01', 'uudenvuodenpäivä'),
     ('2023-01-06', 'loppiainen'),

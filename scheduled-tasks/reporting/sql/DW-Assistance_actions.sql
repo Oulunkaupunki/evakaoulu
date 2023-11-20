38da96ce05a125ce22767f67fb0ed6c373b9815f
Martin Lehtomaa <martin.lehtomaa@rebase.fi>
2023-11-20 10:17:11 +0200

    Made assistance action option values to be flat in DW report

--- scheduled-tasks/reporting/sql/DW-Assistance_actions.sql
+++ scheduled-tasks/reporting/sql/DW-Assistance_actions.sql
@@ -1,17 +1,11 @@
 SELECT
     now() AT TIME ZONE 'Europe/Helsinki'    	AS pvm,
     ac.child_id                                	AS lapsen_id,
-    array(
-        SELECT name_fi
-        FROM assistance_action_option
-        WHERE id IN (
-            SELECT option_id
-            FROM assistance_action_option_ref
-            WHERE action_id = ac.id
-        )
-    )                                    		AS tukitoimi,
+    aao.name_fi                                 AS tukitoimi,
     ac.other_action								as muu_tukitoimi,
     ac.start_date 		                    	AS aloitus_pvm,
     ac.end_date 		                    	AS loppu_pvm
 FROM assistance_action ac
+    LEFT JOIN assistance_action_option_ref aaor ON aaor.action_id = ac.id
+    LEFT JOIN assistance_action_option aao ON aao.id = aaor.option_id
 WHERE :date_val::DATE - INTERVAL '3 years' <= ac.end_date;

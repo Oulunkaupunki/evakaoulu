4e78c81e69f1c58343892dd6fbcc7fa8377c510a
tahtee <96183234+tahtee@users.noreply.github.com>
2023-12-20 12:18:16 +0200

    Enabled employeeMobileConfirmedDaysReservations PROD

--- frontend/oulu/featureFlags.tsx
+++ frontend/oulu/featureFlags.tsx
@@ -108,7 +108,7 @@ const features: Features = {
     noAbsenceType: false,
     employeeMobileStaffAttendanceEdit: true,
     employeeMobileChildAttendanceReservationEdit: true,
-    employeeMobileConfirmedDaysReservations: false
+    employeeMobileConfirmedDaysReservations: true
   }
 }
 

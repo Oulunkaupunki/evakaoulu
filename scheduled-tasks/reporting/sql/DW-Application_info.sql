c3093da56170d4442bf535a01b5518400f29b78e
tahtee <96183234+tahtee@users.noreply.github.com>
2024-10-24 14:11:26 +0300

    Added preferred starting date to DW application info

--- scheduled-tasks/reporting/sql/DW-Application_info.sql
+++ scheduled-tasks/reporting/sql/DW-Application_info.sql
@@ -10,12 +10,13 @@ WITH application_infos AS (
         ap.transferapplication as siirtohakemus,
         ap.child_id as lapsen_id,
         pe.date_of_birth as syntymaaika,
-        jsonb_array_elements_text(ap.document->'apply'->'preferredUnits') AS yksikot
+        jsonb_array_elements_text(ap.document->'apply'->'preferredUnits') AS yksikot,
+        ap.document->'preferredStartDate' AS haluttu_aloituspaiva
     FROM application ap, person pe
     WHERE :date_val::DATE - INTERVAL '12 months' <= ap.created
     AND ap.child_id = pe.id
 ORDER BY ap.created DESC)
-SELECT hakemuksen_id, hakemus_luotu, hakemusta_paivitetty, tyyppi, tilanne, alkupera, siirtohakemus, lapsen_id, syntymaaika, yksikot, dg.name as yksikko_nimi, dg.care_area_id as alue_id, ca.name as alue_nimi
+SELECT hakemuksen_id, hakemus_luotu, hakemusta_paivitetty, tyyppi, tilanne, alkupera, siirtohakemus, lapsen_id, syntymaaika, yksikot, haluttu_aloituspaiva, dg.name as yksikko_nimi, dg.care_area_id as alue_id, ca.name as alue_nimi
 FROM application_infos, daycare dg, care_area ca
 WHERE dg.id IN (application_infos.yksikot::uuid)
   AND dg.care_area_id = ca.id;

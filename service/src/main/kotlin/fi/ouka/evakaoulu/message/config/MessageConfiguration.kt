48175b191b0d3ce8c177abdf5e05ae891a5a6cb3
tahtee <96183234+tahtee@users.noreply.github.com>
2025-05-16 07:42:18 +0300

    Fix build: Added ChildDocumentDecision texts

--- service/src/main/kotlin/fi/ouka/evakaoulu/message/config/MessageConfiguration.kt
+++ service/src/main/kotlin/fi/ouka/evakaoulu/message/config/MessageConfiguration.kt
@@ -104,6 +104,36 @@ If you want to apply for a place other than the one assigned to your child, you 
 If you accept the assigned pre-school place, you don't have to do anything.
                 """
 
+    override fun getChildDocumentDecisionHeader(lang: OfficialLanguage): String =
+        when (lang) {
+            OfficialLanguage.FI -> "Oulun varhaiskasvatukseen liittyvät päätökset"
+            OfficialLanguage.SV -> "Beslut gällande Uleåborgs småbarnspedagogik"
+        }
+
+    override fun getChildDocumentDecisionContent(lang: OfficialLanguage): String =
+        when (lang) {
+            OfficialLanguage.FI ->
+                """
+            Lapsellenne on tehty päätös. Voit katsella päätöstä eVakassa.
+    
+            Koska olette ottanut Suomi.fi -palvelun käyttöönne, on päätös myös luettavissa alla olevista liitteistä.
+            
+            In English:
+            
+            A decision has been made for your child. You can view the decision on eVaka.
+            
+            As you are a user of Suomi.fi, you can also find the decision in the attachments below.
+            """
+                    .trimIndent()
+            OfficialLanguage.SV ->
+                """
+            Beslut har fattats för ditt barn. Du kan se beslutet i eVaka.
+            
+            Eftersom du har tagit Suomi.fi-tjänsten i bruk, kan du också läsa beslutet i bilagorna nedan.
+            """
+                    .trimIndent()
+        }
+
     private fun resolveLocale(lang: OfficialLanguage): Locale {
         if (OfficialLanguage.SV.equals(lang)) return resolveLocale(OfficialLanguage.FI)
         return Locale.of(lang.name.lowercase())

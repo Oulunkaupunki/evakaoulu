169a550153d60e87757a14a375621a348f2367bc
Mikko Savolainen <mikko.savolainen@gofore.com>
2025-07-03 14:47:23 +0300

    refactor e2e to its own folder

---
+++ frontend/lint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2025 City of Turku
+#
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+set -euo pipefail
+
+cd "$(dirname "$0")/../evaka/frontend"; \
+yarn eslint --max-warnings 0 src/lib-customizations/oulu/.

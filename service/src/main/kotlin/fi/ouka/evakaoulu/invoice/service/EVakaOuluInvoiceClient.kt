834b6da95d29d3d8b7a98b8d76a7bc84a56b948d
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2025-10-31 11:06:20 +0200

    Fix flaky invoice tests by using a mock clock

--- service/src/main/kotlin/fi/ouka/evakaoulu/invoice/service/EVakaOuluInvoiceClient.kt
+++ service/src/main/kotlin/fi/ouka/evakaoulu/invoice/service/EVakaOuluInvoiceClient.kt
@@ -7,9 +7,10 @@ package fi.ouka.evakaoulu.invoice.service
 import com.jcraft.jsch.SftpException
 import fi.espoo.evaka.invoicing.domain.InvoiceDetailed
 import fi.espoo.evaka.invoicing.integration.InvoiceIntegrationClient
+import fi.espoo.evaka.shared.domain.EvakaClock
+import fi.espoo.evaka.shared.domain.RealEvakaClock
 import io.github.oshai.kotlinlogging.KotlinLogging
-import java.text.SimpleDateFormat
-import java.util.Date
+import java.time.format.DateTimeFormatter
 
 private val logger = KotlinLogging.logger {}
 
@@ -17,17 +18,22 @@ class EVakaOuluInvoiceClient(
     private val sftpSender: SftpSender,
     private val invoiceGenerator: ProEInvoiceGenerator,
 ) : InvoiceIntegrationClient {
-    override fun send(invoices: List<InvoiceDetailed>): InvoiceIntegrationClient.SendResult {
+    override fun send(invoices: List<InvoiceDetailed>): InvoiceIntegrationClient.SendResult = sendWithClock(invoices, RealEvakaClock())
+
+    fun sendWithClock(
+        invoices: List<InvoiceDetailed>,
+        clock: EvakaClock,
+    ): InvoiceIntegrationClient.SendResult {
         val failedList = mutableListOf<InvoiceDetailed>()
 
         val generatorResult = invoiceGenerator.generateInvoice(invoices)
-        var proEinvoices = generatorResult.invoiceString
+        val proEinvoices = generatorResult.invoiceString
         var successList = generatorResult.sendResult.succeeded
         var manuallySentList = generatorResult.sendResult.manuallySent
 
-        if (!successList.isEmpty()) {
+        if (successList.isNotEmpty()) {
             try {
-                val fileName = SimpleDateFormat("'proe-'yyyyMMdd-hhmmss'.txt'").format(Date())
+                val fileName = "proe-" + clock.now().toLocalDateTime().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss")) + ".txt"
                 sftpSender.send(proEinvoices, fileName)
                 logger.info { "Successfully sent ${successList.size} invoices and created ${manuallySentList.size} manual invoice" }
             } catch (e: SftpException) {
@@ -49,5 +55,5 @@ interface StringInvoiceGenerator {
         val invoiceString: String = "",
     )
 
-    fun generateInvoice(invoices: List<InvoiceDetailed>): StringInvoiceGenerator.InvoiceGeneratorResult
+    fun generateInvoice(invoices: List<InvoiceDetailed>): InvoiceGeneratorResult
 }

0b04e6c381b5827d7abdb80ab9a783443257dd64
Petri Lehtinen <petri.lehtinen@reaktor.com>
2026-04-02 09:42:59 +0300

    Remove Oulu service and frontend code

--- service/src/main/kotlin/fi/ouka/evakaoulu/util/FieldType.kt
+++
@@ -1,9 +0,0 @@
-package fi.ouka.evakaoulu.util
-
-enum class FieldType {
-    ALPHANUMERIC,
-    NUMERIC,
-
-    // we need a specific monetary type because they are prescaled by 100, so they include two decimals
-    MONETARY,
-}
